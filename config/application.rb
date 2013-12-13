cd703b11c827325d65f079dbfd4132ba44a0092b
TheVitruvian <Alexham100@aol.com>
2013-12-13 14:32:42 +0000

    updated config/application.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -34,6 +34,7 @@ module WithHonours
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.initialize_on_precompile = false
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
