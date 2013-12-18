466b67bf97f8db649ab798fc60f226db9c956326
TheVitruvian <Alexham100@aol.com>
2013-12-18 16:48:39 +0000

    More styling

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,24 +5,8 @@ class CompaniesController < ApplicationController
   end
 
   def show
-    @question_up_score = @question_down_score = @answer_up_score = @answer_down_score = @comment_up_score = @comment_down_score = 0
-
     @company= Company.find(params[:id])
-    @company.questions.each do |question|
-      @question_up_score    += question.up_votes_count
-      @question_down_score  += question.down_votes_count
-    end
-
-    @company.answers.each do |answer|
-      @answer_up_score    += answer.up_votes_count
-      @answer_down_score  += answer.down_votes_count
-    end
-
-    @company.comments.each do |comment|
-      @comment_up_score    += comment.up_votes_count
-      @comment_down_score  += comment.down_votes_count
-    end
-
+    @questions = @company.questions.order('hotness DESC')
     @votes = @company.question_votes.count + @company.answer_votes.count + @company.comment_votes.count
   end
 
