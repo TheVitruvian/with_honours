0c35c8c62890fe2fafc1f194d12f2af13f06dfed
TheVitruvian <Alexham100@aol.com>
2014-01-21 16:03:15 +0000

    inbox complete

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
   belongs_to :owner, :polymorphic => true
   belongs_to :recipient, :polymorphic => true
 
-  attr_accessible :owner, :recipient, :content, :is_read, :is_archived, :created_at, :subject
+  attr_accessible :owner, :recipient, :content, :is_read, :is_archived, :created_at, :subject, :owner_id, :owner_type, :recipient_id, :recipient_type
 
 end
